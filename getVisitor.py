bca4262f4ce5f057c1fabd5bc0df0db14f14ec19
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-24 13:08:09 -0400

    Update getVisitor.py

--- getVisitor.py
+++ getVisitor.py
@@ -1,19 +1,21 @@
 import json
 import boto3 
-#imports json and boto3 modules
+#--------------------------------------------imports json and boto3 modules
 dynamodb = boto3.resource('dynamodb') 
-#grabs DynamoDb context
+#--------------------------------------------grabs DynamoDb context
 table = dynamodb.Table('CCdb') 
-#grabs my Ddb table
+#--------------------------------------------grabs my Ddb table
 def lambda_handler(event, context): 
-  #specifies a lambda handler with two arguments
+  #------------------------------------------specifies a lambda handler with two arguments
   response = table.get_item(Key={'Key_id':'0'}) 
-  #grab item by its key id
+  #-------------------------------------------grab item by its key id
+  print(response)
+  #-------------------------------------------print to console
   visitor_count = response['Item']['visitor_count']
-  #save the value of the item as visitor_count
+  #-------------------------------------------save the value of the item as visitor_count
   visitor_count = visitor_count + 1
-  #increment the visitor_count to reflect current page load
-  response = table.put_item(Item={'key_id':'0', 'visitor_count':visitor_count})
-  #update Ddb with new visitor_count value
+  #-------------------------------------------increment the visitor_count to reflect current page load
+  response = table.put_item(Item={'Key_id':'0', 'visitor_count':visitor_count})
+  #-------------------------------------------update Ddb with new visitor_count value
   return visitor_count
-  #return the visitor_count to be handled by JS
+  #-------------------------------------------return the visitor_count to be handled by JS
