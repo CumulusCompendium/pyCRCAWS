02cf85613fe004f978961437df1d81b59ded885f
CumulusCompendium <133039660+CumulusCompendium@users.noreply.github.com>
2023-05-30 20:05:28 -0400

    Update getVisitor.py

--- getVisitor.py
+++ getVisitor.py
@@ -10,7 +10,15 @@ def lambda_handler(event, context):
   response = table.get_item(Key={'Key_id':'0'}) 
   #-------------------------------------------grab item by its key id
   print(response)
-  #-------------------------------------------print to console
+  #-------------------------------------------If response is empty, make visitor_count record starting at 1, return visitor_count
+  if not response.get("Item"):
+      print("null")
+      visitor_count = 1
+      table.put_item(Item={'Key_id':'0', 'visitor_count':visitor_count})
+      return visitor_count
+  else:
+      print("full")
+  #-------------------------------------------if response has an item, save to visitor_count, iterate by 1, and update the record
   visitor_count = response['Item']['visitor_count']
   #-------------------------------------------save the value of the item as visitor_count
   visitor_count = visitor_count + 1
